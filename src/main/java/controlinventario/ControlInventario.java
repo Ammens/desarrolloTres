d85de08840ae96b0f0fd3c68379807e83217b1af
Spectre <Spectre@DESKTOP-BR1UGE3>
2026-03-02 22:55:56 -0700

    Version inicial: ControlInventario

---
+++ src/main/java/controlinventario/ControlInventario.java
@@ -0,0 +1,10 @@
+package controlinventario;
+import vista.VentanaPrincipal;
+
+public class ControlInventario {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
